a5aa551c81cd175197c6816b897b7e9f072f4bc8
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2020-01-07 14:08:15 +0100

    fix: ignore LinkedIn' 999 return code

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml
-bundle exec htmlproofer ./_site
+bundle exec htmlproofer ./_site --http-status-ignore "999"
