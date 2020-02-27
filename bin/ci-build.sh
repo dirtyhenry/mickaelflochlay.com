72d5ea68ecc3cc7a09475af045f866912c6f2eec
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2020-02-27 10:01:25 +0100

    ci: ignore Dailymotion 503 no error by htmlproofer

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -2,4 +2,6 @@
 set -e # halt script on error
 
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml
-bundle exec htmlproofer ./_site --http-status-ignore "999"
+
+# dailymotion returns 503 no error for videos links 🤷‍♂️
+bundle exec htmlproofer --url-ignore "/www.dailymotion.com/" ./_site --http-status-ignore "999"
