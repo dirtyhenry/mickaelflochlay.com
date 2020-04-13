a403d78fbd08e8f641e953b29e4e3bd9851118ed
Mickaël Floc’hlay <mickael.flochlay@margo.com>
2020-04-13 12:48:58 +0200

    ci: let's ignore internal links while the site is offline

--- bin/ci-build.sh
+++ bin/ci-build.sh
@@ -4,4 +4,9 @@ set -e # halt script on error
 JEKYLL_ENV=production bundle exec jekyll build --config _config.yml
 
 # dailymotion returns 503 no error for videos links 🤷‍♂️
-bundle exec htmlproofer --url-ignore "/www.dailymotion.com/" ./_site --http-status-ignore "999"
+# mickaelflochlay.com is offline so let's ignore it while offline
+bundle exec htmlproofer \
+  --url-ignore "/www.dailymotion.com/" \
+  --url-ignore "/mickaelflochlay.com/" \
+  --http-status-ignore "999" \
+  ./_site 
